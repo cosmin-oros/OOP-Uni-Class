80bdc5a5e9e0b1cdda0e529fa2435186c56413c7
cosmin-oros <oroscosmin09@gmail.com>
2023-03-08 15:01:40 +0200

    extra

---
+++ src/Extra/Test1.java
@@ -0,0 +1,31 @@
+package Extra;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class Test1 {
+
+    public static void reverse(List<Integer> list) {
+        Collections.reverse(list);
+        System.out.println(list);
+    }
+
+    private static void myRecursiveFunction(int n) {
+        if (n == 0) {
+            return;
+        }
+        System.out.println(n);
+        myRecursiveFunction(n-1);
+    }
+
+    public static void main(String[] args) {
+        myRecursiveFunction(10);
+
+        List<Integer> list = new ArrayList<>();
+        list.add(1);
+        list.add(2);
+
+        reverse(list);
+    }
+}
